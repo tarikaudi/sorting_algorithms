f9a89dd10a5fffad3ea6e244ab96e2aa2b8adb07
defrancescomartin <defrancescoma@gmail.com>
2022-06-08 18:53:27 -0300

    0 Bubble sort strcture

---
+++ 0-bubble_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sort an array using Bubble Sort algorithm
+ * in ascending order
+ * @array: array of integers to sort
+ * @size: length of the array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+
+}
