0233b47a47323e5bff4ec7d879c3e50a4e0df0aa
defrancescomartin <defrancescoma@gmail.com>
2022-06-09 15:59:14 -0300

    insertion sort task 1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * insertion_sort - Sort an array using Insertion Sort algorithm
+ * in ascending order
+ * @array: array of integers to sort
+ * @size: length of the array
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *tmp;
+
+	if (!list)
+		return;
+
+	for (current = *list; current; current = current->next)
+	{
+		tmp = current;
+		while (tmp->prev && (tmp->n < tmp->prev->n))
+		{
+			tmp->prev->next = tmp->next;
+			if (tmp->next) 
+			    tmp->next->prev = tmp->prev;
+			tmp->next = tmp->prev;
+			tmp->prev = tmp->prev->prev;
+			tmp->next->prev = tmp;
+			if (tmp->prev)
+				tmp->prev->next = tmp;
+			else
+				*list = tmp;
+			print_list(*list);
+		}
+	}
+}
