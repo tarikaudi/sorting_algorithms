a2a5dce295d12b1c7c30162ecf2f2f9a0a673869
defrancescomartin <defrancescoma@gmail.com>
2022-06-11 17:06:19 -0300

    quick sort algorithm finished

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -58,7 +58,7 @@ void quicksort(int *array, int low, int high, size_t size)
     int p = 0;
 	if (low < high)
 	{
-		int p = partition(array, low, high, size);
+		p = partition(array, low, high, size);
 		quicksort(array, low, p - 1, size);
 		quicksort(array, p + 1, high, size);
 	}
