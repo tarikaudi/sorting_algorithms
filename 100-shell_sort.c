f6510b64d876821b7d710d3a7da6e800c1bc69e8
defrancescomartin <defrancescoma@gmail.com>
2022-06-12 17:25:37 -0300

    shell sort with knuth secuence

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,9 +1,8 @@
 #include "sort.h"
-
 /**
-  * shell_sort - shell sort algorithm
-  * @array: given array
-  * @size: size of the array
+  * shell_sort - shell sort
+  * @array: array
+  * @size: size
   */
 
 void shell_sort(int *array, size_t size)
@@ -22,16 +21,16 @@ void shell_sort(int *array, size_t size)
 		gap = (gap - 1) / 3;
 		for (i = 0; i < size - 1; i++)
 		{
-			k = i;
+			r = i;
 			for (j = i + gap; j < size; j += gap)
 			{
-				if (array[j] < array[r])
+				if (array[j] < array[k])
 				{
 					k = j;
 				}
 			}
-			aux = array[r];
-			array[r] = array[i];
+			aux = array[k];
+			array[k] = array[i];
 			array[i] = aux;
 		}
 		print_array(array, size);
